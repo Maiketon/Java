642da96a2a8848b6839f957b300e6c52bdcbdf8d
Maiketon <miky_lee24@hotmail.com>
2026-07-13 14:59:30 -0600

    Ejercicio DetallePersona
    Se crea una simulacion de un "Objeto" con tipos de datos primivitos y objetos.

---
+++ Variables/src/DetallePersona.java
@@ -0,0 +1,18 @@
+public class DetallePersona {
+
+
+    static void main(String[] args) {
+        String nombreCompleto="Miguel Angel Montoya Bautista";
+        int edad=25;
+        double altura=1.71;
+        String paisOrigen="Mexico";
+        char estadoCivil='S';
+
+        System.out.println(nombreCompleto);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(estadoCivil);
+
+    }
+}
