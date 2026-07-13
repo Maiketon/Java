d9e0d8823a352763ed0ed13c45841745e52e6825
Maiketon <miky_lee24@hotmail.com>
2026-07-13 14:22:22 -0600

    Se crea el proyecto Variables
    Se crean las primeras variables primitivas.

---
+++ Variables/src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        //Variables
+
+        int edad = 31;
+        double precio=300.40;
+        boolean disponible = true;
+        char genero;
+
+        //Asignacion de gnero  M masculino F Feminino
+
+        genero = 'M';
+
+        //Para mandar a imprimir las variables hay qu eacceder a ellas
+
+        System.out.println(edad);
+
+        //modificando el valor de edad
+
+        edad= 35;
+        System.out.println(edad);
+    }
+}
