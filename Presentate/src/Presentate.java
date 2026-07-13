bfd2bc3f72ab111d6c773b498fa9cc80e940e75e
Maiketon <miky_lee24@hotmail.com>
2026-07-13 13:24:17 -0600

    Ejercicio Presentate
    Se realiza el ejercicio Presentate con las notaciones psvm y sout, para escribir Mi nombre, edad y pais

---
+++ Presentate/src/Presentate.java
@@ -0,0 +1,8 @@
+public class Presentate {
+
+    public static void main(String[] args) {
+        System.out.println("Miguel Angel Montoya Bautista");
+        System.out.println("24 años");
+        System.out.println("México");
+    }
+}
