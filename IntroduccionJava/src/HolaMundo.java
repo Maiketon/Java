fbeb228cf95dabc70712b90e395968910e4d758b
Maiketon <miky_lee24@hotmail.com>
2026-07-13 13:19:52 -0600

    Se crea el proyecto HolaMundo
    Se crea el proyecto Hola mundo, en el cual concoemos los atajos sout

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    //Poner comentarios con doble diagonal//
+
+    //Agregamos el metodo prinicial, conocido como main//
+    public static void main(String[] args) {
+        //El arreglo principal es un arreglo para agregar cualquier parametro que entre//
+        System.out.println("Hola Mundo con Java");
+        System.out.println("Saludos !!");
+    }
+
+}
