7617a351a069fa5d23c948f8258d00017a8c05da
Maiketon <miky_lee24@hotmail.com>
2026-07-15 14:00:47 -0600

    Ejercicio De datos
    Se realiza el ejercicio de "Reserva Hoteles" con inferencia de datos , buenas practicas en la sintaxis.

---
+++ Variables/src/ReservaHoteles.java
@@ -0,0 +1,28 @@
+public class ReservaHoteles {
+
+    static void main(String[] args) {
+        System.out.println("Sistema de Reserva de Hoteles");
+
+        //Definicion de Variables
+
+        var nombreCliente ="Miguel Montoya";
+        var diasEstancia = 7;
+        var tarifaDiaria= 1300.00:
+        var tieneVistaMar =true;
+
+        //////////////////////////////////////////////////
+        System.out.println("nombreCliente = " + nombreCliente);
+        System.out.println("diasEstancia = " + diasEstancia);
+        System.out.println("tarifaDiaria = " + tarifaDiaria);
+        System.out.println("tieneVistaMar = " + tieneVistaMar);
+        //Modificacion de valores
+
+        diasEstancia=5;
+        tarifaDiaria=900.00;
+        tieneVistaMar=false;
+        
+
+
+
+    }
+}
